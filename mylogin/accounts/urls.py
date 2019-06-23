ca7aac54fe4700daec68321860752fff2cabfcac
Sandeep <ssharma@iitk.ac.in>
2019-06-23 19:27:55 +0530

    linked with homepage

---
+++ mylogin/accounts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('signup/', views.signup_view, name='signup'),
+    path('signedup/', views.signup_index , name='signup_index'), 
+    path('login/', views.login_view , name='login'), 
+    path('loggedin/', views.login_index , name='login_index'),
+    path('logout/', views.logout_view, name='logout'),
+    path('home/', views.home, name="home"),
+    path('',views.base,name="base")
+]
---
+++ mylogin/accounts/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
+from django.http import HttpResponse
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.decorators import login_required
+
+loggedin = 'false'
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            loggedin = 'true'
+            login(request, user)
+            return redirect('accounts:login_index')
+    else:
+        form = UserCreationForm()
+    return render(request,'accounts/signup.html',{'form':form})
+
+def signup_index(request):
+    return HttpResponse("You are signed up")
+
+def login_view(request):
+        
+        global loggedin
+
+        if loggedin is 'true':
+            return redirect('accounts:login_index')
+    
+        else:    
+            if request.method == 'POST':
+                form = AuthenticationForm(data=request.POST)
+                if form.is_valid():
+                    #log the user in
+                    user = form.get_user()
+                    loggedin = 'true'
+                    login(request, user)
+                    return redirect('accounts:login_index') 
+
+            else:
+                form = AuthenticationForm()
+            return render(request, 'accounts/login.html', {'form':form})
+    
+
+
+@login_required(login_url='/accounts/login/')
+def login_index(request):
+    return render(request, 'accounts/loggedin.html')
+
+def logout_view(request):
+    global loggedin
+    if request.method == 'POST':
+        loggedin = 'false'
+        logout(request)
+        return redirect('accounts:home')
+
+def home(request):
+    return render(request, 'accounts/homepage.html')
+
+def base(request):
+    return render(request, 'accounts/base.html')
+
+# Create your views here.
