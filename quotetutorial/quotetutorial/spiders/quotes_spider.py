22735ec1e46fdff97f85304ded55ce3a34dd5124
nidhihegde001 <43878053+nidhihegde001@users.noreply.github.com>
2019-06-29 17:38:47 +0530

    scrapes multiple pages of the website

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -1,14 +1,14 @@
-
 import  scrapy
 from ..items import QuotetutorialItem
 
 class Quotespider(scrapy.Spider):
 
     name = 'quotes'
-
+    page_number = 2
     start_urls = [
         'http://quotes.toscrape.com/'
     ]
+
 
     def parse(self,response):
         item = QuotetutorialItem()
@@ -25,3 +25,10 @@ class Quotespider(scrapy.Spider):
             item['tag'] = tag
 
             yield item
+
+        next_page = 'http://quotes.toscrape.com/page/'+str(Quotespider.page_number)+'/'
+        #response.css('li.next a::attr(href)').get()
+        #if next_page is not None:
+        if  Quotespider.page_number<=10:
+            Quotespider.page_number+=1
+            yield response.follow(next_page, callback = self.parse)
